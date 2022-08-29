69b1cb570d00abb9eb73eb15243e36f76b23a2a9
Cristian Ferreyra <cristianbferreyra@gmail.com>
2022-08-28 21:10:19 -0300

    desafio librerías terminado

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let tituloMenu = document.createElement("h2");
 tituloMenu.id = "menu__title";
 tituloMenu.innerHTML = "Elegí tu gustito del día";
 divContenedor.appendChild(tituloMenu);
+
 const productos = [
   {
     id: 1,
@@ -21,14 +22,11 @@ const productos = [
     precio: 1000,
   },
   { id: 3, imagen: "chapignon.jpeg", name: "Champignon", precio: 1600 },
-  {
-    id: 4,
-    imagen: "chapignon.jpeg",
-    name: "Especial Albahaca",
-    precio: 1300,
-  },
+  { id: 4, imagen: "chapignon.jpeg", name: "Especial Albahaca", precio: 1300 },
   { id: 5, imagen: "napo.jpg", name: "Napolitana", precio: 1350 },
+  { id: 6, imagen: "chapignon.jpeg", name: "Fugazzeta", precio: 1200 },
 ];
+let carrito = [];
 for (let cardItem of productos) {
   let cards = document.createElement("div");
   cards.className = "cardsStyle";
@@ -36,60 +34,56 @@ for (let cardItem of productos) {
   />
     <h3 class = "card__title"> ${cardItem.name}</h3> 
     <p class = "card__precio" >  $${cardItem.precio}</p>
+    <button class= "comprar__btn" id= "${cardItem.id}">Comprar</button>
+    <div class = "confirmacion_carrito"></div>
     `;
   seccion[0].appendChild(cards);
 }
-let card = document.getElementsByClassName("cardsStyle");
-console.log(card);
-let boton1 = document.createElement("button");
-let boton2 = document.createElement("button");
-let boton3 = document.createElement("button");
-let boton4 = document.createElement("button");
-let boton5 = document.createElement("button");
-boton1.id = "1";
-boton2.id = "2";
-boton3.id = "3";
-boton4.id = "4";
-boton5.id = "5";
-card[0].appendChild(boton1);
-card[1].appendChild(boton2);
-card[2].appendChild(boton3);
-card[3].appendChild(boton4);
-card[4].appendChild(boton5);
-console.log(boton1);
-boton1.innerHTML = "Comprar";
-boton2.innerHTML = "Comprar";
-boton3.innerHTML = "Comprar";
-boton4.innerHTML = "Comprar";
-boton5.innerHTML = "Comprar";
 
-let idBoton1 = parseInt(boton1.id);
-console.log(idBoton1);
-/* function buscarProducto(id) {
-  return productos.find((elemento) => elemento.id == id);}
+//captura del botón  y el div de confirmacion de agragado al carrito en el DOM
+let botonesComprar = document.querySelectorAll(".comprar__btn");
+let confirmacionCarrito = document.querySelector(".confirmacion_carrito");
+//recorro los botones de los productos y les asigno el evento para que al hacer click, agregue los productos al carrito
+botonesComprar.forEach((botonComprar) => {
+  botonComprar.addEventListener("click", (e) => {
+    let idBoton = parseInt(e.target.id); //acá el id pasa de string a número
+    let productoEncontrado = buscarProducto(idBoton); //en base al número (orieginalmente del id del botón) busca el producto en el array
+    agregarProducto(productoEncontrado); //pushea el objeto (producto) al array (carrito)
+    guardaCarrito(carrito);
 
-
-function avisoCompra() {
-  console.log("Enviado al carrito!");
+    /*JUSTIFICACIÓN DE LIBRERÍA: Elegí usar Toastify porque me parece la librería más apropiada para este caso en el que sólo necesito que aparezca un mensaje breve que notifique al usuario que se agregó el producto al carrito */
+    Toastify({
+      text: `La pizza de ${productoEncontrado.name} fue agregada al carrito de compras`,
+      duration: 4000,
+      destination: "https://github.com/apvarun/toastify-js",
+      close: true,
+      gravity: "top",
+      position: "right",
+      style: {
+        color: "#FF5733",
+        background: "linear-gradient(to right,#FBFCFC , #F8F9F9 )",
+      },
+    }).showToast();
+    //confirmacionCarrito.innerHTML = `La pizza de: ${productoEncontrado.name} fue agregada al carrito de compras`;
+    /*  console.log(
+      `La pizza de: ${productoEncontrado.name} fue agregada al carrito de compras`
+    ); */
+  });
+});
+function buscarProducto(id) {
+  return productos.find((elemento) => elemento.id == id);
 }
-
-boton1.addEventListener("click", avisoCompra);
- */
-/* function agregarProducto(producto) {
+function agregarProducto(producto) {
   carrito.push(producto);
   console.log(carrito);
-} */
-//fx buscar elemento por el id
-/* function buscarProducto(id) {
-  return productos.find((elemento) => elemento.id == id);
-} */
-/* <button class ="card__btn">Comprar</button> */
-//let productoElegido = buscarProducto();
-/* boton1.addEventListener("click", buscarProducto(parseInt(boton1.className)));
-let productoElegido = buscarProducto(); */
-//fx busca el producto en el array(objeto) por el id y lo guarda en producatoElegido
-//console.log(productoElegido); //muestro el objeto que encontró la fx por el id
+}
 
-/* agregarProducto(productoElegido); //AGREGO EL OBJETO (Producto) al array CARRITO
-console.log(carrito);
- */
+//HASTA ACÁ EL CARRITO FUNCIONANDO DEVUELVE ARRAY EN CONSOLA
+
+function carritoRecuperar() {
+  JSON.parse(localStorage.getItem("carrito")) || [];
+}
+
+function guardaCarrito(carrito) {
+  localStorage.setItem("carritoJson", JSON.stringify(carrito));
+}
